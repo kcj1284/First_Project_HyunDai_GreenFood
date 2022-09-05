ed18305c640be8cb7f95170f6f8c6207ba0627a5
CJ.KIM <kcj1284@gmail.com>
2022-09-05 10:57:06 +0900

    [#23] 경로 수정 및 out 폴더 삭제

--- HyunDai_GreenFood_First_project/src/com/kosa/jdbc/DbUtil.java
+++
@@ -1,8 +0,0 @@
-package com.kosa.jdbc;
-
-public class DbUtil {
-	public static String driver = "oracle.jdbc.driver.OracleDriver";
-	public static String url = "jdbc:oracle:thin:@edudb_high?TNS_ADMIN=C:\\\\dev\\\\OracleWallet\\\\Wallet_edudb";
-	public static String user = "hdgf";
-	public static String password = "xxxxxxAt21cc";
-}
