0462db1d506ff8d2127799b88c00ab9cbc67bd84
chanjung kim <kcj1284@gmail.com>
2022-09-10 23:42:28 +0900

    [#77] IR_Center_DAO & VO 초기 설정

--- src/com/hdgf/dao/IR_Center_DAO.java
+++ src/com/hdgf/dao/IR_Center_DAO.java
@@ -18,31 +18,31 @@ public class IR_Center_DAO {
 	public static IR_Center_DAO getInstance() {
 		return instance;
 	}
-	
+
 	public void insertUsers(IR_Center_VO IR_Center_VO) {
-	Connection conn = null;
-	PreparedStatement pstmt = null;
-	ResultSet rset = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
 
-	String runSP = "{ call sp_insert_IR_Center(?, ?, ?, ?, ?, ?) }";
+		String runSP = "{ call sp_insert_IR_Center(?, ?, ?, ?, ?, ?) }";
 
-	try {
-		conn = DBConnection.getConnection();
-		CallableStatement callableStatement = conn.prepareCall(runSP);
-		callableStatement.setString(1, IR_Center_VO.getTitle());
-		callableStatement.setString(2, IR_Center_VO.getUser_id());
-		callableStatement.setDate(3, IR_Center_VO.getWrdate());
-		callableStatement.setString(4, IR_Center_VO.getMain_text());
-		callableStatement.setString(5, IR_Center_VO.getFile_link());
-		callableStatement.setInt(6, IR_Center_VO.getVisiter());
-		callableStatement.executeUpdate();
-		System.out.println("성공");
-	} catch (SQLException e) {
-		System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-		e.printStackTrace();
-	} catch (Exception e) {
-		e.printStackTrace();
+		try {
+			conn = DBConnection.getConnection();
+			CallableStatement callableStatement = conn.prepareCall(runSP);
+			callableStatement.setString(1, IR_Center_VO.getTitle());
+			callableStatement.setString(2, IR_Center_VO.getUser_id());
+			callableStatement.setDate(3, IR_Center_VO.getWrdate());
+			callableStatement.setString(4, IR_Center_VO.getMain_text());
+			callableStatement.setString(5, IR_Center_VO.getFile_link());
+			callableStatement.setInt(6, IR_Center_VO.getVisiter());
+			callableStatement.executeUpdate();
+			System.out.println("성공");
+		} catch (SQLException e) {
+			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-
-}
 }
