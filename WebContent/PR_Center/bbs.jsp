db1c25f2ee865036bd96dd8c4da72467f78fb3e5
chanjung kim <kcj1284@gmail.com>
2022-09-11 00:12:49 +0900

    [#77] IR_Center_DAO & VO 생성

--- WebContent/PR_Center/bbs.jsp
+++ WebContent/PR_Center/bbs.jsp
@@ -11,7 +11,7 @@
 <!-- 화면 최적화 -->
 <meta name="viewport" content="width-device-width", initial-scale="1">
 <!-- 루트 폴더에 부트스트랩을 참조하는 링크 -->
-<link rel="stylesheet" href="../css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap.css">
 <title>JSP 게시판 웹 사이트</title>
 <style type="text/css">
 	a, a:hover{
--- src/com/hdgf/dao/AnnouncementDAO.java
+++ src/com/hdgf/dao/AnnouncementDAO.java
@@ -1,12 +1,8 @@
 package com.hdgf.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import com.hdgf.dto.AnnouncementVO;
-import com.hdgf.util.DBConnection;
 
 public class AnnouncementDAO {
 
@@ -19,27 +15,9 @@ public class AnnouncementDAO {
 		return instance;
 	}
 	
-	
-	
 	public ArrayList<AnnouncementVO> listAnnouncement() {
-		ArrayList<AnnouncementVO> annList = new ArrayList<AnnouncementVO>();
-		String sql = "select * from announcement";
+		ArrayList<AnnouncementVO> announcementList = new ArrayList<AnnouncementVO>();
 		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = DBConnection.getConnection();
-			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				AnnouncementVO annVO = new AnnouncementVO();
-				annVO.setId(rs.getInt("BOARD_ID"));
-				annVO.setTitle(rs.getString("TITLE"));
-				annVO.setU_id(rs.getString(""));
-			}
-		}
 	}
 	
 }
