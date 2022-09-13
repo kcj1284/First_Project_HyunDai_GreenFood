758a01bd760cb634322d8f3caffb82ebe54c27de
chanjung kim <kcj1284@gmail.com>
2022-09-13 16:58:13 +0900

    [#94] IR_Center_DAO callable로 수정

--- src/com/hdgf/dao/IR_Center_DAO.java
+++ src/com/hdgf/dao/IR_Center_DAO.java
@@ -19,31 +19,67 @@ public class IR_Center_DAO {
 		return instance;
 	}
 
-	public void insertUsers(IR_Center_VO IR_Center_VO) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rset = null;
+	Connection conn = null;
 
-		String runSP = "{ call sp_insert_IR_Center(?, ?, ?, ?, ?, ?, ?) }";
+	public void insert_IR_Center(IR_Center_VO IR_Center_VO) {
+
+		String runSP = "{ call sp_insert_IR_Center(?, ?, ?, ?, ?) }";
 
 		try {
 			conn = DBConnection.getConnection();
 			CallableStatement callableStatement = conn.prepareCall(runSP);
 			// callableStatement.setInt(1, IR_Center_VO.getIR_id());
-			callableStatement.setString(2, IR_Center_VO.getTitle());
-			callableStatement.setString(3, IR_Center_VO.getUser_id());
-			callableStatement.setDate(4, IR_Center_VO.getWrdate());
-			callableStatement.setString(5, IR_Center_VO.getMain_text());
-			callableStatement.setString(6, IR_Center_VO.getFile_link());
-			callableStatement.setInt(7, IR_Center_VO.getVisiter());
+			callableStatement.setString(1, IR_Center_VO.getTitle());
+			callableStatement.setString(2, IR_Center_VO.getUser_id());
+			callableStatement.setString(3, IR_Center_VO.getMain_text());
+			callableStatement.setString(4, IR_Center_VO.getFile_link());
 			callableStatement.executeUpdate();
-			System.out.println("����");
+			System.out.println("success");
 		} catch (SQLException e) {
 			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
+
+	public void update_IR_Center(IR_Center_VO IR_Center_VO) {
+
+		String runSP = "{ call sp_update_IR_Center(?, ?, ?) }";
+
+		try {
+			conn = DBConnection.getConnection();
+			CallableStatement callableStatement = conn.prepareCall(runSP);
+			callableStatement.setInt(1, IR_Center_VO.getIR_id());
+			callableStatement.setString(2, IR_Center_VO.getTitle());
+			callableStatement.setString(3, IR_Center_VO.getMain_text());
+			callableStatement.setString(4, IR_Center_VO.getFile_link());
+			callableStatement.executeUpdate();
+			System.out.println("success");
+		} catch (SQLException e) {
+			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void delete_IR_Center(IR_Center_VO IR_Center_VO) {// 삭제는 dto 전부 받을 필요없음.
+
+		String runSP = "{ call sp_delete_IR_Center(?) }";
+
+		try {
+			conn = DBConnection.getConnection();
+			CallableStatement callableStatement = conn.prepareCall(runSP);
+			callableStatement.setInt(1, IR_Center_VO.getIR_id());
+			callableStatement.executeUpdate();
+			System.out.println("success");
+		} catch (SQLException e) {
+			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
