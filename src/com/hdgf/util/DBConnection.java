db7344b5aa20bdea588015556d6b55e36d69ad27
chanjung kim <kcj1284@gmail.com>
2022-09-18 16:41:12 +0900

    [#191] DBCp Tets

--- src/com/hdgf/util/DBConnection.java
+++ src/com/hdgf/util/DBConnection.java
@@ -1,13 +1,8 @@
 package com.hdgf.util;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBConnection {
 
