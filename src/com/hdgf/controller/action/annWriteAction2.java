860666c788d3c5dae9c778ff741e8d346db9e2ba
wkdwndus0409 <jjy990409@naver.com>
2022-09-18 14:06:42 +0900

    [#184] 공지소식 글쓰기 -> 파일 업로드, 다운로드 기능 추가

---
+++ src/com/hdgf/controller/action/annWriteAction2.java
@@ -0,0 +1,69 @@
+package com.hdgf.controller.action;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import com.oreilly.servlet.MultipartRequest;
+
+import com.hdgf.dao.AnnouncementDAO;
+import com.hdgf.dao.FileDAO;
+import com.hdgf.dao.QnaDAO;
+import com.hdgf.dto.AnnouncementVO;
+import com.hdgf.dto.QnaVO;
+import com.hdgf.dto.UsersVO;
+
+public class annWriteAction2 implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+		String url = "HdgfServlet?command=notice";
+
+		HttpSession session = request.getSession();
+		UsersVO loginUser = (UsersVO) session.getAttribute("loginUser");
+
+		// 파일 첨부 기능 코드
+		String directory = request.getSession().getServletContext().getRealPath("/upload/");
+
+		File targetDir = new File(directory);
+		if (!targetDir.exists()) {
+			targetDir.mkdirs();
+		}
+
+		int maxSize = 1024 * 1024 * 500;
+		String encoding = "UTF-8";
+		
+		MultipartRequest multipartRequest = new MultipartRequest(request, directory, maxSize, encoding,
+				new DefaultFileRenamePolicy());
+		
+		String fileName = multipartRequest.getOriginalFileName("file");
+		String fileRealName = multipartRequest.getFilesystemName("file");
+		
+		FileDAO fileDAO = FileDAO.getInstance();
+		fileDAO.upload(fileName, fileRealName);
+		int fileId = fileDAO.getFileId(fileRealName);
+
+		AnnouncementDAO annDAO = AnnouncementDAO.getInstance();
+
+		if (loginUser == null) {
+			url = "HdgfServlet?command=loginForm";
+		} else {
+			AnnouncementVO annVO = new AnnouncementVO();
+			annVO.setTitle(multipartRequest.getParameter("title"));
+			annVO.setU_id(loginUser.getUser_id());
+			annVO.setMain_text(multipartRequest.getParameter("main_text"));
+			annVO.setfile_id(fileId);
+			annVO.setAnnoun_type(Integer.parseInt(multipartRequest.getParameter("ann_type")));
+			annDAO.write(annVO);
+		}
+
+		response.sendRedirect(url);
+	}
+}
---
+++ src/com/hdgf/controller/action/downloadAction.java
@@ -0,0 +1,62 @@
+package com.hdgf.controller.action;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hdgf.dao.FileDAO;
+import com.hdgf.dto.FileVO;
+
+public class downloadAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, SQLException {
+
+		int fileId = Integer.parseInt(request.getParameter("file"));
+
+		FileDAO fileDAO = FileDAO.getInstance();
+		FileVO fileVO = fileDAO.getFile(fileId);
+
+		String directory = request.getServletContext().getRealPath("/upload/");
+		File file = new File(directory + "/" + fileVO.getFileRealName());
+
+		String mimeType = request.getServletContext().getMimeType(file.toString());
+		if (mimeType == null) {
+			response.setContentType("application/octet-stream");
+		}
+
+		String downloadName = null;
+
+		if (request.getHeader("user-agent").indexOf("MSIE") == -1) {
+			downloadName = new String(fileVO.getFileRealName().getBytes("UTF-8"), "8859_1");
+		} else {
+			downloadName = new String(fileVO.getFileRealName().getBytes("EUC-KR"), "8859_1");
+		}
+
+		response.setHeader("Content-Disposition", "attachment;filename=\""+ downloadName + "\";");
+		
+		FileInputStream fileInputStream = new FileInputStream(file);
+		ServletOutputStream servletOutputStream = response.getOutputStream();
+		
+		byte b[] = new byte[1024];
+		int data = 0;
+		
+		while((data = (fileInputStream.read(b,0,b.length))) != -1) {
+			servletOutputStream.write(b,0,data);
+		}
+		
+		servletOutputStream.flush();
+		servletOutputStream.close();
+		fileInputStream.close();
+	}
+
+}
