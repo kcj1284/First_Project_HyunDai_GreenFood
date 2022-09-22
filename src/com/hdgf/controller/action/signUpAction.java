d8033dc7f4107f8980f9dba88be79f16a5d2a4c7
nsmchan <ehfhfh1313@naver.com>
2022-09-23 08:41:18 +0900

    [#216] 회원가입 액션의 세션생성코드 삭제

--- src/com/hdgf/controller/action/signUpAction.java
+++ src/com/hdgf/controller/action/signUpAction.java
@@ -22,20 +22,17 @@ public class signUpAction implements Action {
 			throws ServletException, IOException, SQLException {
 		String url = "/login/login.jsp";
 
-		HttpSession session = request.getSession();
-
 		UsersVO usersVO = new UsersVO();
 
 		usersVO.setUser_id(request.getParameter("user_id"));
 		usersVO.setUser_pw(request.getParameter("user_pw"));
 		usersVO.setUser_name(request.getParameter("user_name"));
-		usersVO.setTel(request.getParameter("tel"));
+		usersVO.setTel(request.getParameter("tel"));	
 		usersVO.setEmail(request.getParameter("email"));
 		usersVO.setGender(Integer.parseInt(request.getParameter("gender")));
 		usersVO.setAdministrator(0);	// 홈페이지에서 가입하는사람은 일반계정만 // 1은 관리자
 		usersVO.setCom_type(Integer.parseInt(request.getParameter("com_type")));
-
-		session.setAttribute("user_id", request.getParameter("user_id"));
+		
 
 		UsersDAO userDAO = UsersDAO.getInstance();
 		userDAO.insertUsers(usersVO);
